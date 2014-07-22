10eb4651b143526567931eaeef7254557e20a41e
jonatasemidio@gmail.com <>
2014-07-22 08:19:10 -0300

    save param

--- src/PARAM.py
+++ src/PARAM.py
@@ -5,8 +5,10 @@ disk = 'D:'
 serv = '***' #inserir o nome do servidor CVS
 repo = '***' #Inserir o nome do repositorio
 
-# Inserir a string de conex�o com o CVS
-#Ex: ':sspi;username=<<usuario>>;password=<<senha>>;hostname=<<ip da maquina em quest�o>>:/<<nome do repositorio>>'
+
+
+# Inserir a string de conexão com o CVS
+#Ex: ':sspi;username=<<usuario>>;password=<<senha>>;hostname=<<ip da maquina em questão>>:/<<nome do repositorio>>'
 CVSROOT = ''
 
 	
